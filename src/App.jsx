4a5b6ac8919d1864f5ed50e2735f16c4127cb2ae
Allam9573 <aallamdaniel@gmail.com>
2023-12-11 18:02:23 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Login } from "./components/Login"
+function App() {
+  return (
+    <div>
+      <h1>Hola React Vite</h1>
+      <Login user="Allam" />
+    </div>
+  )
+}
+export { App }
